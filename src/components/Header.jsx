093a352bc043a8db846a2fdfd90a0588f7f88e66
root <132725436+tingtung46@users.noreply.github.com>
2024-12-15 18:40:14 +0700

    Add Header component (html skeleton)

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <nav className="flex space-between">
+          <div>
+            <img src="" alt="Logo" />
+          </div>
+
+          <div>
+            <ul>
+              <li>Home</li>
+              <li>Shop</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+
+          <div>
+            <img src="" alt="Cart Icon" />
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
