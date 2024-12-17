c20a7368efe4282266ee3dc10952b7d00cfae7f9
root <132725436+tingtung46@users.noreply.github.com>
2024-12-17 18:35:30 +0700

    Add Footer component (html skeleton)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="container small">
+          <p>
+            &copy; Copyright 2025 Alpinus. All rights reserved. Made with ❤️ by{" "}
+            <a href="https://github.com/tingtung46">Tingtung</a>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
