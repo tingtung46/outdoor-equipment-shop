46436db95ff433541453586305125f1635caad57
root <132725436+tingtung46@users.noreply.github.com>
2024-12-19 19:33:54 +0700

    Add Category component (html skeleton)

---
+++ src/components/Category.jsx
@@ -0,0 +1,20 @@
+export const Category = () => {
+  const productCategory = [];
+
+  return (
+    <>
+      <section>
+        <h2>Category</h2>
+
+        {productCategory.map((category) => {
+          <div>
+            <div>
+              <img src="" alt="Category" />
+              <p>{category}</p>
+            </div>
+          </div>;
+        })}
+      </section>
+    </>
+  );
+};
