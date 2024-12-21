9637986df7de279d05be3d2f6ffccd7fcec4949d
root <132725436+tingtung46@users.noreply.github.com>
2024-12-21 10:38:39 +0700

    Add ReasonBuying component (html skeleton)

---
+++ src/components/ReasonBuying.jsx
@@ -0,0 +1,35 @@
+export const ReasonBuying = () => {
+  return (
+    <>
+      <section>
+        <h2>Why shop at Alpinus</h2>
+
+        <div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <h3>Store pickup</h3>
+              <p>Many items ready same day</p>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h3>Easy returns</h3>
+              <p>Shop with confidence</p>
+            </div>
+            <img src="" alt="" />
+          </div>
+
+          <div>
+            <img src="" alt="" />
+            <div>
+              <h3>Free Shipping</h3>
+              <p>On most items</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
