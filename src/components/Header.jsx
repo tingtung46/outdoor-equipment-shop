d02cc08b2e97ac8f6a6ef55aa31470cae7ff1e9e
root <132725436+tingtung46@users.noreply.github.com>
2024-12-23 17:57:59 +0700

    Add home page (html skeleton)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,30 @@
 import logo from "../images/logo/navbar-logo.png";
 import { ShoppingCart } from "lucide-react";
 
-export const Header = () => {
-  return (
-    <>
-      <header>
-        <div className="container">
-          <div>
-            <img src={logo} alt="Logo" />
-          </div>
+export const Header = ({ children }) => {
+	return (
+		<>
+			<header>
+				<div className="container">
+					<div>
+						<img src={logo} alt="Logo" />
+					</div>
 
-          <nav className="flex">
-            <ul>
-              <li>Home</li>
-              <li>Shop</li>
-              <li>About Us</li>
-            </ul>
-          </nav>
+					<nav className="flex">
+						<ul>
+							<li>Home</li>
+							<li>Shop</li>
+							<li>About Us</li>
+						</ul>
+					</nav>
 
-          <div>
-            <ShoppingCart />
-          </div>
-        </div>
-      </header>
-    </>
-  );
+					<button type="button">
+						<ShoppingCart />
+					</button>
+				</div>
+
+				{children}
+			</header>
+		</>
+	);
 };
