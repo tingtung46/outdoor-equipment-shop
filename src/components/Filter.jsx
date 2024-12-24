3e69ac0fe3607d9e9ba02686ef541fccf8e69abb
root <132725436+tingtung46@users.noreply.github.com>
2024-12-24 16:29:13 +0700

    Add Filter component (html skeleton)

---
+++ src/components/Filter.jsx
@@ -0,0 +1,40 @@
+import { FilterIcon } from "lucide-react";
+import { types, brands } from "../data/filterItem";
+
+export const Filter = () => {
+	return (
+		<>
+			<h2>
+				Filter <FilterIcon />
+			</h2>
+
+			<div>
+				<p>Type</p>
+
+				<button type="button">All</button>
+
+				{types.map((type) => {
+					return (
+						<button type="button" key={type.id}>
+							{type.type}
+						</button>
+					);
+				})}
+			</div>
+
+			<div>
+				<p>Brand</p>
+
+				<button type="button">All</button>
+
+				{brands.map((brand) => {
+					return (
+						<button type="button" key={brand.id}>
+							{brand.brand}
+						</button>
+					);
+				})}
+			</div>
+		</>
+	);
+};
