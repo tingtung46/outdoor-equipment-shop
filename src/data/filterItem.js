09c5c81456946a786f09593a2cf7406f6195d51b
root <132725436+tingtung46@users.noreply.github.com>
2024-12-24 16:29:54 +0700

    Add filter data

---
+++ src/data/filterItem.js
@@ -0,0 +1,89 @@
+export const types = [
+	{
+		id: 1,
+		type: "Footwear",
+	},
+	{
+		id: 2,
+		type: "Climbing",
+	},
+	{
+		id: 3,
+		type: "Ski/boarding",
+	},
+	{
+		id: 4,
+		type: "Bags",
+	},
+	{
+		id: 5,
+		type: "Trekking",
+	},
+	{
+		id: 6,
+		type: "Jackets",
+	},
+	{
+		id: 7,
+		type: "Navigation",
+	},
+	{
+		id: 8,
+		type: "Cycling",
+	},
+];
+
+export const brands = [
+	{
+		id: 1,
+		brand: "Daybird",
+	},
+	{
+		id: 2,
+		brand: "Gravitator",
+	},
+	{
+		id: 3,
+		brand: "WildRunner",
+	},
+	{
+		id: 4,
+		brand: "Quester",
+	},
+	{
+		id: 5,
+		brand: "B&R",
+	},
+	{
+		id: 6,
+		brand: "Raptor Elite",
+	},
+	{
+		id: 7,
+		brand: "Solstix",
+	},
+	{
+		id: 8,
+		brand: "Grolltex",
+	},
+	{
+		id: 9,
+		brand: "AirStrider",
+	},
+	{
+		id: 10,
+		brand: "Green Equipment",
+	},
+	{
+		id: 11,
+		brand: "Zephyr",
+	},
+	{
+		id: 12,
+		brand: "Legend",
+	},
+	{
+		id: 13,
+		brand: "XE",
+	},
+];
