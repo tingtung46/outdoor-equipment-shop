2f5043b3e44fa3195aacba48e6f4349f73c38340
root <132725436+tingtung46@users.noreply.github.com>
2024-12-29 11:33:37 +0700

    Update product and filter data

---
+++ src/data/filterState.js
@@ -0,0 +1,33 @@
+export const filterState = {
+  searchTerm: '',
+  passingTags: {
+    search: {
+      inputTerm: '',
+    },
+    Type: {
+      Footwear: false,
+      Climbing: false,
+      'Ski/boarding': false,
+      Bags: false,
+      Trekking: false,
+      Jackets: false,
+      Navigation: false,
+      Cycling: false,
+    },
+    Brand: {
+      Daybird: false,
+      Gravitator: false,
+      WildRunner: false,
+      Quester: false,
+      'B&R': false,
+      'Raptor Elite': false,
+      Solstix: false,
+      Grolltex: false,
+      AirStrider: false,
+      'Green Equipment': false,
+      Zephyr: false,
+      Legend: false,
+      XE: false,
+    },
+  },
+};
