61e10ffaae1c598f6b75e6ab36a5bb5d365bb3ee
root <132725436+tingtung46@users.noreply.github.com>
2025-01-08 10:15:44 +0700

    Set link for each category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { type } from '../data/filterItem';
 import { getCategoryImage } from '../utils/getImage';
+import { Link } from 'react-router-dom';
 
 const Category = () => {
   return (
@@ -12,8 +13,10 @@ const Category = () => {
           {type.map((category) => {
             return (
               <div key={uuidv4()}>
-                <img src={getCategoryImage(category.id)} alt="Category" />
-                <p>{category.type}</p>
+                <Link to={`/shop-page/${category.id}`}>
+                  <img src={getCategoryImage(category.id)} alt="Category" />
+                  <p>{category.type}</p>
+                </Link>
               </div>
             );
           })}
