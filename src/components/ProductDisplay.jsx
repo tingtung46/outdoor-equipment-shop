27504669ff411f34dbb882cd87c1a2495d395ec4
root <132725436+tingtung46@users.noreply.github.com>
2025-01-13 11:42:31 +0700

    Fix pagination when select another category

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -1,18 +1,25 @@
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Card from './Card';
 import Pagination from './Pagination';
 import PropTypes from 'prop-types';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { getData } from '../utils/productDisplayManager';
 
 let pageSize = 9;
 
 const ProductDisplay = () => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+  const navigate = useNavigate();
   const { category } = useParams();
 
   const products = getData(category);
+
+  const onPageChange = (onPage) => {
+    setSearchParams({ page: onPage });
+    navigate(`/shop-page/${category}?page=${encodeURIComponent(onPage)}`);
+  }
 
   const currentProductData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * pageSize;
@@ -38,7 +45,7 @@ const ProductDisplay = () => {
                 currentPage={currentPage}
                 totalCount={products.length}
                 pageSize={pageSize}
-                onPageChange={(page) => setCurrentPage(page)}
+                onPageChange={onPageChange}
               />
             </div>
           </section>
