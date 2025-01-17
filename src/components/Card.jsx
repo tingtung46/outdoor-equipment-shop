3d74bdce07258ac68f1b68b18a5843a1818b3f33
root <132725436+tingtung46@users.noreply.github.com>
2025-01-17 21:52:08 +0700

    Set link in card to product page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { getProductImage } from '../utils/getImage';
+import { Link } from 'react-router-dom';
 
 const Card = ({ product }) => {
   const imgUrl = getProductImage(product.Id);
@@ -7,22 +8,26 @@ const Card = ({ product }) => {
   return (
     <>
       <div className="w-25 h-27">
-        <img src={imgUrl} alt="Product" className="w-full h-full rounded-md" />
+        <Link
+          to={`/product/${product.Type.toLowerCase()}/${product.Name.split(' ').join('-').toLowerCase()}`}
+        >
+          <img src={imgUrl} alt="Product" className="w-full h-full rounded-md" />
 
-        <div className="flex justify-between">
-          <p>{product.Name}</p>
-          <p>
-            {'\u0024'}
-            {product.Price}
-          </p>
-        </div>
+          <div className="flex justify-between">
+            <p>{product.Name}</p>
+            <p>
+              {'\u0024'}
+              {product.Price}
+            </p>
+          </div>
+        </Link>
       </div>
     </>
   );
 };
 
 Card.propTypes = {
-  product: PropTypes.any || PropTypes.array
+  product: PropTypes.any || PropTypes.array,
 };
 
 export default Card;
