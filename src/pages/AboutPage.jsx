ee054bb3823c0c1736423fb8a6ddbd77bce4c2b7
root <132725436+tingtung46@users.noreply.github.com>
2025-01-18 12:32:15 +0700

    Add about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,21 @@
+const AboutPage = () => {
+  return (
+    <>
+      <p>
+        Welcome to Alpinus, your trusted destination for all things outdoors! Born out of a passion
+        for adventure and a love for nature, we strive to equip adventurers, explorers and nature
+        enthusiasts with the best gear and resources for their journeys.
+      </p>
+
+      <p>
+        At Alpinus, we believe that the great outdoors is for everyone. Whether you are scaling
+        mountain peaks, camping, under starry skies or simply enjoying a weekend hike, we are here
+        to support you with high-quality equipment, expert advice and commitment to sustainability.
+      </p>
+
+      <p>Gear up and get out there – adventure awaits.</p>
+    </>
+  );
+};
+
+export default AboutPage;
