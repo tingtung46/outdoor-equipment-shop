f6e1b68241ca275abea9a1fd10bb934a421075a4
root <132725436+tingtung46@users.noreply.github.com>
2025-01-21 16:55:03 +0700

    Set empty cart component if shopping cart is empty

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,30 +2,41 @@ import { v4 as uuidv4 } from 'uuid';
 import CartItem from '../components/CartItem';
 import GetProductButton from '../components/GetProductButton';
 import OrderSummary from '../components/OrderSummary';
+import emptyCart from '../images/heroImages/empty-cart.png';
 import PropTypes from 'prop-types';
 
 const CartPage = ({ shoppingCart, updateProduct, removeProduct }) => {
-  return (
-    <>
-      <section>
-        {shoppingCart.map((item) => {
-          return (
-            <CartItem
-              key={uuidv4()}
-              item={item}
-              updateProduct={updateProduct}
-              removeProduct={removeProduct}
-            />
-          );
-        })}
-      </section>
+  console.log(shoppingCart)
+  if (!shoppingCart.length) {
+    return (
+      <div>
+        <img src={emptyCart} alt="Empty Cart" />
+        <p>The cart is empty</p>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <section>
+          {shoppingCart.map((item) => {
+            return (
+              <CartItem
+                key={uuidv4()}
+                item={item}
+                updateProduct={updateProduct}
+                removeProduct={removeProduct}
+              />
+            );
+          })}
+        </section>
 
-      <GetProductButton />
-      <OrderSummary />
+        <GetProductButton />
+        <OrderSummary />
 
-      <button type="button">Check Out</button>
-    </>
-  );
+        <button type="button">Check Out</button>
+      </>
+    )
+  }
 };
 
 CartPage.propTypes = {
