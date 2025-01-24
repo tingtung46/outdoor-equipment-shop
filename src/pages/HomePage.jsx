bfa865ffd8c85b9c16a95a2501a3382162156d07
root <132725436+tingtung46@users.noreply.github.com>
2025-01-24 12:32:20 +0700

    Edit style of hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,13 +5,13 @@ import hero from '../images/heroImages/hero.png';
 const HomePage = () => {
   return (
     <>
-      <section className="relative h-screen overflow-hidden">
-        <div className="absolute inset-0">
+      <section className="hero relative h-[300px] sm:h-[320px] md:h-[392px] lg:h-[512px] overflow-hidden w-full">
+        <div className="absolute inset-0 h-auto w-full">
           <img src={hero} alt="Hero" />
           <div className="absolute inset-0 bg-black opacity-50" />
         </div>
 
-        <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
+        <div className="relative z-10 flex flex-col justify-end lg:justify-center items-center lg:items-start lg:pl-8 h-full text-center text-stone-300 text-sm pb-6 lg:pb-0 sm:text-base lg:text-lg">
           <h1>Ready for adventure?</h1>
           <p>Create lifetime memories and enjoy experience to the fullest</p>
         </div>
