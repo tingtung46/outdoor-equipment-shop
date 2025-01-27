47ccb6fb8419592d29ecae3d7e24f750a780f348
root <132725436+tingtung46@users.noreply.github.com>
2025-01-27 10:01:21 +0700

    Add animation to nav mobile

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,8 +1,8 @@
-import { useClickAway } from "react-use";
-import { useRef, useState } from "react";
-import { Squash as Hamburger } from "hamburger-react";
-import { AnimatePresence, motion } from "motion/react";
-import { Link } from "react-router-dom";
+import { useClickAway } from 'react-use';
+import { useRef, useState } from 'react';
+import { Squash as Hamburger } from 'hamburger-react';
+import { AnimatePresence, motion } from 'motion/react';
+import { Link } from 'react-router-dom';
 
 const NavMobile = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,26 +12,69 @@ const NavMobile = () => {
 
   return (
     <div ref={ref} className="md:hidden">
-      <Hamburger toggled={isOpen} size={20} toggle={setIsOpen} />
-      {isOpen && (
-        <nav className="fixed left-0 shadow-xl right-0 pb-5 z-20 bg-white/80">
-          <ul className="grid">
-            <li className="border-t-2 border-stone-400 py-1 px-5 hover:bg-stone-400/60">
-              <Link onClick={() => setIsOpen((prev) => !prev)} to="/" className="text-neutral-500 font-semibold">Home</Link>
-            </li>
+      <div className="text-neutral-500">
+        <Hamburger toggled={isOpen} size={20} toggle={setIsOpen} />
+      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.nav
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="fixed left-0 shadow-xl right-0 z-20 bg-white/60"
+          >
+            <ul className="grid">
+              <motion.li
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ type: 'spring', stiffness: 260, damping: 20, delay: 0.1 }}
+                className="py-2 px-5 mx-7 hover:bg-stone-400/60"
+              >
+                <Link
+                  onClick={() => setIsOpen((prev) => !prev)}
+                  to="/"
+                  className="text-neutral-500 font-semibold"
+                >
+                  Home
+                </Link>
+              </motion.li>
 
-            <li className="border-t-2 border-stone-400 py-1 px-5 hover:bg-stone-400/60">
-              <Link onClick={() => setIsOpen((prev) => !prev)} to="/shop-page/all" className="text-neutral-500 font-semibold">Shop</Link>
-            </li>
+              <motion.li
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ type: 'spring', stiffness: 260, damping: 20, delay: 0.1 }}
+                className="border-t-2 border-gray-400/60 py-2 px-5 mx-7 hover:bg-stone-400/60"
+              >
+                <Link
+                  onClick={() => setIsOpen((prev) => !prev)}
+                  to="/shop-page/all"
+                  className="text-neutral-500 font-semibold"
+                >
+                  Shop
+                </Link>
+              </motion.li>
 
-            <li className="border-t-2 border-stone-400 py-1 px-5 hover:bg-stone-400/60">
-              <Link onClick={() => setIsOpen((prev) => !prev)} to="/about" className="text-neutral-500 font-semibold">About Us</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
+              <motion.li
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ type: 'spring', stiffness: 260, damping: 20, delay: 0.1 }}
+                className="border-t-2 border-gray-400/60 py-2 px-5 mx-7 hover:bg-stone-400/60"
+              >
+                <Link
+                  onClick={() => setIsOpen((prev) => !prev)}
+                  to="/about"
+                  className="text-neutral-500 font-semibold"
+                >
+                  About Us
+                </Link>
+              </motion.li>
+            </ul>
+          </motion.nav>
+        )}
+      </AnimatePresence>
     </div>
-  )
-}
+  );
+};
 
 export default NavMobile;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,13 +7,15 @@ const HomePage = () => {
     <>
       <section className="relative mt-[-3rem] h-screen w-full">
         <div className="absolute inset-0 overflow-hidden h-auto w-full">
-          <img src={hero} alt="Hero" className='w-full h-full object-cover' />
+          <img src={hero} alt="Hero" className="w-full h-full object-cover" />
           <div className="absolute inset-0 left-0 bottom-0 bg-gradient-to-b from-white/75 from-10% to-transparent to-90%" />
         </div>
 
-        <div className="relative z-10 flex flex-col justify-end lg:justify-center items-center lg:items-start lg:pl-8 lg:ml-20 h-full text-center text-gray-500 text-sm pb-6 lg:pb-0 sm:text-base lg:text-lg">
-          <h1>Ready for adventure?</h1>
-          <p>Create lifetime memories and enjoy experience to the fullest</p>
+        <div className="relative z-10 flex flex-col justify-end lg:justify-center items-center lg:items-start lg:pl-8 lg:ml-20 h-full text-center text-sm pb-20 lg:pb-0 sm:text-base lg:text-lg">
+          <h1 className="text-neutral-600 lg:text-gray-500">Ready for adventure?</h1>
+          <p className="text-gray-800 lg:text-gray-500">
+            Create lifetime memories and enjoy experience to the fullest
+          </p>
         </div>
       </section>
       <Category />
