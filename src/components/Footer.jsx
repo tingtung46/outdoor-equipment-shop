c1198b44f857077d1827fc62a0e52ea5177aa371
root <132725436+tingtung46@users.noreply.github.com>
2025-01-28 13:55:34 +0700

    Style footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div className="container small">
-          <p>
-            &copy; Copyright 2025 Alpinus. All rights reserved. Made with ❤️ by{" "}
-            <a href="https://github.com/tingtung46">Tingtung</a>
-          </p>
-        </div>
+      <footer className="flex flex-column items-center justify-center bg-neutral-800 py-2">
+        <p className="text-gray-400 text-sm">
+          &copy; Copyright 2025 Alpinus. All rights reserved. Made with ❤️ by{' '}
+          <a href="https://github.com/tingtung46" className="text-gray-400 hover:text-white">
+            Tingtung
+          </a>
+        </p>
       </footer>
     </>
   );
