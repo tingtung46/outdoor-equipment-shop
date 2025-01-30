3ad8ed2fa3884367894333eb0f7cbdaeec476276
root <132725436+tingtung46@users.noreply.github.com>
2025-01-30 18:35:20 +0700

    Style desktop filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,8 +1,9 @@
-import { ListFilter } from 'lucide-react';
 import PropTypes from 'prop-types';
 import { type, brand } from '../data/filterItem';
 import getBrandFilter from '../utils/handleFilter';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import FilterDesktop from './FilterDesktop';
+import FilterMobile from './FilterMobile';
 
 const Filter = ({ handleFilter }) => {
   const { category } = useParams();
@@ -10,44 +11,9 @@ const Filter = ({ handleFilter }) => {
 
   return (
     <>
-      <aside>
-        <h2>
-          Filter <ListFilter />
-        </h2>
-
-        <div>
-          <p>Category</p>
-
-          <ul>
-            {type.map((type) => {
-              return (
-                <li key={type.id}>
-                  <Link to={`/shop-page/${type.param}`}>{type.type}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        <div>
-          <p>Brand</p>
-
-          <div>
-            {brands.map((brand) => {
-              return (
-                <div key={brand.id}>
-                  <input
-                    type="checkbox"
-                    name={brand.brand.toLowerCase()}
-                    id={brand.brand.toLowerCase()}
-                    onChange={(e) => handleFilter(e, brand.brand.toLowerCase())}
-                  />
-                  <label htmlFor={brand.brand.toLowerCase()}>{brand.brand}</label>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+      <aside className="bg-white md:border-r-2 md:border-neutral-300 shadow-md md:pl-6 md:w-[250px]">
+        <FilterMobile type={type} brands={brands} handleFilter={handleFilter} />
+        <FilterDesktop type={type} brands={brands} handleFilter={handleFilter} />
       </aside>
     </>
   );
