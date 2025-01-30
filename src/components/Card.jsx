9d5637122887412988e947e09ad3c21ed83fc896
root <132725436+tingtung46@users.noreply.github.com>
2025-01-30 18:37:51 +0700

    Style product card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,18 +7,17 @@ const Card = ({ product }) => {
 
   return (
     <>
-      <div className="w-25 h-27">
-        <Link
-          to={`/product/${product.Param}/${product.Name.split(' ').join('-').toLowerCase()}`}
-        >
-          <img src={imgUrl} alt="Product" className="w-full h-full rounded-md" />
-
-          <div className="flex justify-between">
-            <p>{product.Name}</p>
-            <p>
-              {'\u0024'}
-              {product.Price}
-            </p>
+      <div className="w-25 h-27 border-2 border-neutral-300 rounded-xl p-3 shadow-md bg-white">
+        <Link to={`/product/${product.Param}/${product.Name.split(' ').join('-').toLowerCase()}`}>
+          <div>
+            <img src={imgUrl} alt="Product" className="w-full h-full rounded-md mb-2" />
+            <div className="flex justify-between md:text-sm">
+              <p>{product.Name}</p>
+              <p>
+                {'\u0024'}
+                {product.Price}
+              </p>
+            </div>
           </div>
         </Link>
       </div>
