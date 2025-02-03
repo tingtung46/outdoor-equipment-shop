c37f34fd79ea4382c938e803b2f989154fc66e12
root <132725436+tingtung46@users.noreply.github.com>
2025-02-03 10:31:56 +0700

    Center the text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <>
       <footer className="flex flex-column items-center justify-center bg-neutral-800 py-2">
-        <p className="text-gray-400 text-sm">
+        <p className="text-gray-400 text-sm text-center">
           &copy; Copyright 2025 Alpinus. All rights reserved. Made with ❤️ by{' '}
           <a href="https://github.com/tingtung46" className="text-gray-400 hover:text-white">
             Tingtung
