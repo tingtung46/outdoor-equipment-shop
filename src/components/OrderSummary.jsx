ab05b6e71cdc6d110c060358761046bd218616ec
root <132725436+tingtung46@users.noreply.github.com>
2025-02-10 08:29:01 +0700

    Style order summary component

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,28 +1,36 @@
 const OrderSummary = ({ subTotal }) => {
   return (
-    <section>
-      <h3>Order Summary</h3>
+    <section className="flex flex-col items-center my-12">
+      <div className="w-[90%] lg:w-[83%]">
+        <h3 className="text-lg sm:text-xl font-semibold mb-3 bg-gray-200 px-2 py-3">
+          Order Summary
+        </h3>
 
-      <div>
-        <div>
-          <p>Sub total</p>
-          <p>{'\u0024'}{subTotal}</p>
+        <div className="mb-5 px-2">
+          <div className="flex justify-between">
+            <p>Sub total</p>
+
+            <p>
+              {'\u0024'}
+              {subTotal}
+            </p>
+          </div>
+
+          <div className="flex justify-between">
+            <p>Estimated taxes</p>
+
+            <p>--</p>
+          </div>
         </div>
 
-        <div>
-          <p>Standard shipping</p>
-          <p>FREE</p>
-        </div>
+        <div className="flex justify-between px-2">
+          <p>Order Total</p>
 
-        <div>
-          <p>Estimated taxes</p>
-          <p>--</p>
+          <p className="text-lg font-semibold">
+            {'\u0024'}
+            {subTotal}
+          </p>
         </div>
-      </div>
-
-      <div>
-        <p>Order Total</p>
-        <p>{'\u0024'}{subTotal}</p>
       </div>
     </section>
   );
