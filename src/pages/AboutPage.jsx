32fb6bbf1b64e84ab38baa6df6d905eba020e46e
root <132725436+tingtung46@users.noreply.github.com>
2025-02-11 15:04:54 +0700

    Style about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,20 +1,45 @@
+import img1 from '../images/aboutImages/about1.png';
+import img2 from '../images/aboutImages/about2.png';
+
 const AboutPage = () => {
   return (
-    <>
-      <p>
-        Welcome to Alpinus, your trusted destination for all things outdoors! Born out of a passion
-        for adventure and a love for nature, we strive to equip adventurers, explorers and nature
-        enthusiasts with the best gear and resources for their journeys.
-      </p>
+    <div className="h-min-screen">
+      <div className="flex flex-col items-center">
+        <h1 className="my-10">Who we are</h1>
 
-      <p>
-        At Alpinus, we believe that the great outdoors is for everyone. Whether you are scaling
-        mountain peaks, camping, under starry skies or simply enjoying a weekend hike, we are here
-        to support you with high-quality equipment, expert advice and commitment to sustainability.
-      </p>
+        <div className="block w-[90%] mb-12">
+          <img
+            src={img1}
+            alt="Trekking"
+            className="w-[213.3px] h-[142.3px] sm:w-[426.6px] sm:h-[284.6px] md:w-[640px] md:h-[427px] rounded-md mb-5 mx-auto"
+          />
 
-      <p>Gear up and get out there – adventure awaits.</p>
-    </>
+          <p className="mb-5">
+            Welcome to Alpinus, your trusted destination for all things outdoors! Born out of a
+            passion for adventure and a love for nature, we strive to equip adventurers, explorers
+            and nature enthusiasts with the best gear and resources for their journeys.
+          </p>
+
+          <p>
+            At Alpinus, we believe that the great outdoors is for everyone. Whether you are scaling
+            mountain peaks, camping, under starry skies or simply enjoying a weekend hike, we are
+            here to support you with high-quality equipment, expert advice and commitment to
+            sustainability.
+          </p>
+        </div>
+
+        <div className="w-[90%] mb-12">
+          <img
+            src={img2}
+            alt="Climbing"
+            className="w-[213.3px] h-[142.3px] sm:w-[426.6px] sm:h-[284.6px] md:w-[640px] md:h-[427px] rounded-md mb-5 mx-auto"
+          />
+          <p className="text-center italic">
+            <span className="font-bold text-lg">Gear up and get out there</span> – adventure awaits.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
