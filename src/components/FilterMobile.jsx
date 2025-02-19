3512690926be9377ae71d5bbf7cc91d89dad7597
root <132725436+tingtung46@users.noreply.github.com>
2025-02-19 19:34:02 +0700

    Adjust size of filter to be able to scroll when the size is not fit on viewport

--- src/components/FilterMobile.jsx
+++ src/components/FilterMobile.jsx
@@ -39,6 +39,7 @@ const FilterMobile = ({ type, brands, handleFilter, paramBrands }) => {
       >
         <ListFilter />
       </button>
+
       <AnimatePresence>
         {isOpen && (
           <motion.div
@@ -46,76 +47,80 @@ const FilterMobile = ({ type, brands, handleFilter, paramBrands }) => {
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: "-100%" }}
             transition={{ duration: 0.5 }}
-            className="absolute left-0 shadow-xl right-0 z-20 bg-white overflow-y-auto h-screen p-6"
+            className="absolute left-0 shadow-xl right-0 z-20 bg-white overflow-y-scroll h-screen"
           >
-            <div>
-              <h3 className="font-semibold text-lg mb-2">Category</h3>
+            <div className="overflow-y-auto h-[86%] p-6">
+              <div>
+                <h3 className="font-semibold text-lg mb-2">Category</h3>
 
-              <ul>
-                {type.map((type) => {
-                  return (
-                    <li key={type.id} className="mb-1">
-                      <Link
-                        to={`/shop-page/${type.param}`}
-                        onClick={() => setIsOpen((prev) => !prev)}
-                        className="text-neutral-600 text-base"
-                      >
-                        {type.type}
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-
-            <div>
-              <h3 className="font-semibold text-lg mt-3 mb-2">Brand</h3>
+                <ul>
+                  {type.map((type) => {
+                    return (
+                      <li key={type.id} className="mb-1">
+                        <Link
+                          to={`/shop-page/${type.param}`}
+                          onClick={() => setIsOpen((prev) => !prev)}
+                          className="text-neutral-600 text-base"
+                        >
+                          {type.type}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
 
               <div>
-                {brands.map((brand) => {
-                  return (
-                    <div
-                      key={brand.id}
-                      className="flex gap-2 items-center mb-1"
-                    >
-                      <input
-                        type="checkbox"
-                        name={brand.brand.toLowerCase()}
-                        id={brand.brand.toLowerCase()}
-                        value={brand.brand}
-                        checked={
-                          brand.brand.toLowerCase() ===
-                          paramBrands.find(
-                            (item) => item === brand.brand.toLowerCase()
-                          )
-                        }
-                        onChange={(e) => {
-                          handleFilter(e, brand.brand.toLowerCase());
-                          handleSelect(e);
-                        }}
-                        className="peer appearance-none shrink-0 w-4 h-4 border-2 border-gray-300 rounded-sm bg-white focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-gray-200 checked:bg-gray-500 checked:border-0"
-                      />
-                      <label
-                        htmlFor={brand.brand.toLowerCase()}
-                        className="text-neutral-600 text-base"
+                <h3 className="font-semibold text-lg mt-3 mb-2">Brand</h3>
+
+                <div>
+                  {brands.map((brand) => {
+                    return (
+                      <div
+                        key={brand.id}
+                        className="flex gap-2 items-center mb-1"
                       >
-                        {brand.brand}
-                      </label>
-                      <svg
-                        className="absolute w-4 h-4 pointer-events-none hidden peer-checked:block stroke-white mt-1 outline-none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <polyline points="20 6 9 17 4 12"></polyline>
-                      </svg>
-                    </div>
-                  );
-                })}
+                        <input
+                          type="checkbox"
+                          name={brand.brand.toLowerCase()}
+                          id={brand.brand.toLowerCase()}
+                          value={brand.brand}
+                          checked={
+                            brand.brand.toLowerCase() ===
+                            paramBrands.find(
+                              (item) => item === brand.brand.toLowerCase()
+                            )
+                          }
+                          onChange={(e) => {
+                            handleFilter(e, brand.brand.toLowerCase());
+                            handleSelect(e);
+                          }}
+                          className="peer appearance-none shrink-0 w-4 h-4 border-2 border-gray-300 rounded-sm bg-white focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-gray-200 checked:bg-gray-500 checked:border-0"
+                        />
+
+                        <label
+                          htmlFor={brand.brand.toLowerCase()}
+                          className="text-neutral-600 text-base"
+                        >
+                          {brand.brand}
+                        </label>
+
+                        <svg
+                          className="absolute w-4 h-4 pointer-events-none hidden peer-checked:block stroke-white mt-1 outline-none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <polyline points="20 6 9 17 4 12"></polyline>
+                        </svg>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
           </motion.div>
